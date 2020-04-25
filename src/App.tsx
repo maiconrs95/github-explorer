ab5c8a7cb818004397655701decff006adb39b1a
Maicon Silva <maiconrs95@gmail.com>
2020-04-25 14:50:11 -0300

    add: styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/Global';
 import Routes from './routes';
 
 const App: React.FC = () => (
     <BrowserRouter>
         <Routes />
+        <GlobalStyle />
     </BrowserRouter>
 );
 
