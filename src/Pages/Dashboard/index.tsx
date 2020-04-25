60e53ce3a875e991585c6827cf5f0735b79bae20
Maicon Silva <maiconrs95@gmail.com>
2020-04-25 20:08:14 -0300

    update: dashboard styles

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/github-logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+    return (
+        <>
+            <img src={logoImg} alt="Github explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/19610095?s=460&u=871596faa48f02d9b589d1e072d6dca9f7dc265c&v=4"
+                        alt="maicon silva"
+                    />
+                    <div>
+                        <strong>Maicon Silva</strong>
+                        <p>subtitle</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
