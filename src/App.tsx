c61f00dfc8749e0696b3f70d4d716fda43c5548d
Maicon Silva <maiconrs95@gmail.com>
2020-04-25 14:37:00 -0300

    add: config router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-function App() {
-    return (
-        <div className="App">
-            APP
-        </div>
-    );
-}
+const App: React.FC = () => (
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+);
 
 export default App;
