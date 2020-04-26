6a34e939440c560a605f3d6b27b88dc8d0f5a536
Maicon Silva <maiconrs95@gmail.com>
2020-04-26 17:27:39 -0300

    add: repository page integration with github api

--- src/Pages/Repository/index.tsx
+++ src/Pages/Repository/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-
 import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/github-logo.svg';
 
@@ -11,57 +11,95 @@ interface RepositoryParams {
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
+interface Issue {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    };
+}
+
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
+
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
+
+    useEffect(() => {
+        api.get(`/repos/${params.repository}`).then(({ data }) =>
+            setRepository(data),
+        );
+
+        api.get(`/repos/${params.repository}/issues`).then(({ data }) =>
+            setIssues(data),
+        );
+    }, [params.repository]);
 
     return (
         <>
             <Header>
                 <img src={logoImg} alt="Github Explorer" />
-                <Link to="/dashboard">
+                <Link to="/">
                     <FiChevronsLeft size={16} />
                     Voltar
                 </Link>
             </Header>
 
-            <RepositoryInfo>
-                <header>
-                    <img src="https://avatars3.githubusercontent.com/u/19610095?s=460&u=871596faa48f02d9b589d1e072d6dca9f7dc265c&v=4" />
+            {repository && (
+                <RepositoryInfo>
+                    <header>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
 
-                    <div>
-                        <strong>maiconrs95/maiconsilva</strong>
-                        <p>descrição do repositório</p>
-                    </div>
-                </header>
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
 
-                <ul>
-                    <li>
-                        <strong>1808</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>48</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>67</strong>
-                        <span>Issues abertas</span>
-                    </li>
-                </ul>
-            </RepositoryInfo>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues abertas</span>
+                        </li>
+                    </ul>
+                </RepositoryInfo>
+            )}
 
             <Issues>
-                <Link
-                    key="repository.full_name"
-                    to={`/repository/${'repository.full_name'}`}
-                >
-                    <div>
-                        <strong>repository.owner.login</strong>
-                        <p>repository.description</p>
-                    </div>
+                {issues.map((issue) => (
+                    <a key={issue.id} href={issue.html_url} target="blank">
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
 
-                    <FiChevronRight size={20} />
-                </Link>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Issues>
         </>
     );
